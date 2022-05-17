b5d76e3698f3da8e476aefb824bc5b3d6bdd7acd
JustinMonty20 <montgomerycodepro@gmail.com>
2022-05-16 22:47:45 -0400

    adding in ws npm package to handle being able to connect to websocket's in Node.js

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Testing 123...");
+// websocket connection to interact with Kraken API
+import WebSocket from "ws";
+const krakenSocket = new WebSocket("wss://ws.kraken.com");
+
+krakenSocket.onopen = (event) => {
+    console.log(event);
+    console.log("establishing connection...");
+}
+
+
