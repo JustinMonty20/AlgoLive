fbe15c7a0de318bba8ed417a87e7f43dde0bbde5
JustinMonty20 <montgomerycodepro@gmail.com>
2022-05-16 23:05:20 -0400

    prototype websocket implementation with kraken websockets

--- index.ts
+++ index.ts
@@ -3,8 +3,23 @@ import WebSocket from "ws";
 const krakenSocket = new WebSocket("wss://ws.kraken.com");
 
 krakenSocket.onopen = (event) => {
-    console.log(event);
     console.log("establishing connection...");
+
 }
 
+krakenSocket.onmessage = (event) => {
+    const eventObject = JSON.parse(event.data.toString())
+    console.log(eventObject);
+    if(eventObject.status === "online") {
+        krakenSocket.send(payload)
+    }
+}
+
+const payload = JSON.stringify({
+    event: "subscribe",
+    pair: ["ALGO/USD"],
+    subscription: {
+        name: "ticker",
+    }
+});
 
