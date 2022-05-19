23428b693c22bfb014e082a9653b29e66216b48c
JustinMonty20 <montgomerycodepro@gmail.com>
2022-05-18 23:38:42 -0400

    subscribe & system status type

--- types/krakenTypes.ts
+++ types/krakenTypes.ts
@@ -1,9 +1,23 @@
 // types of payload data we can get back from Kraken Websockets
 export type PublicMessages = "ticket" | "ohlc" | "trade" | "spread" | "book";
 
+// represents a system status message sent back from Kraken Websockets api
 export type SystemStatus = {
-    connectionId: string,
+    connectionId?: string,
     event: "systemStatus",
     status: "online" | "maintenance" | "cancel_only" | "limit_only" | "post_only",
-    version: "string"
+    version: string
+}
+
+// represents a subscribe status message that is sent back from Karken Websockets api
+export type Subscribe = {
+    event: "subscribe",
+    reqid?: number,
+    pair?: string[],
+    subscription: {
+        depth?: 10|25|100|500|1000
+        // interval is only used for ohlc (open high low close type)
+        interval?: 1|5|15|30|60|240|1440|10080|21600
+        name: PublicMessages,
+    }
 }
