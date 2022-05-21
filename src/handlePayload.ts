db7a25e4457a3325800d934c456e6e3ae70c873d
Justin Montgomery <montgomerycodepro@gmail.com>
2022-05-20 20:42:46 -0400

    refactor old behavior to into new handleEvent function

--- src/handlePayload.ts
+++ src/handlePayload.ts
@@ -1,19 +1,24 @@
-import {Client} from "discord.js";
+import { Client } from "discord.js";
 import WebSocket from "ws";
 import { DataSource, PublicMessages } from "../types";
 
 
-let currCandleTime: number;
+// let currCandleTime: number;
 
 export default (_client: Client, ds: DataSource) => {
     // client will be passed down to update the nickname and status further on down the line.
     ds.ws.onmessage = (event) => {
-        handleEvent(event)
+        handleEvent(event,ds)
     }
 }
 
-const handleEvent = (event: WebSocket.MessageEvent) => {
-    console.log("calling handleEvent");
+const handleEvent = (event: WebSocket.MessageEvent, ds: DataSource) => {
+    const eventMsgData = JSON.parse(event.data.toString());
+    console.log(eventMsgData);
+    if(eventMsgData.status == "online") {
+        ds.ws.send(subscribePayload("ohlc"))
+    }
+    
 }
 
 // subscribe to ohlc (open high low close data from Kraken WebSocket API)
